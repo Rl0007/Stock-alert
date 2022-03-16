8d3461a4c2086e68d14dea281e7c1d92504abab7
Rahul <12agrawalrahul@gmail.com>
2022-03-16 18:57:39 +0530

    try5

--- Api/api.py
+++ Api/api.py
@@ -51,7 +51,8 @@ def eventhandlerstart():
 
 @app.route('/')
 def serve():
-    return send_from_directory(app.static_folder,'index.html')
+    # return send_from_directory(app.static_folder,'index.html')
+    return{'202':'hello'}
 
 # stockup backend
 def stockup_serializer(stockup):
